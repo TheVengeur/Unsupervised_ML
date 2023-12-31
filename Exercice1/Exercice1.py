428d12b7b97a733e7662acd624ee88ca80e1d10b
TheVengeur <45660215+TheVengeur@users.noreply.github.com>
2023-12-31 01:03:23 +0100

    Add Exercice 3

--- Exercice1/Exercice1.py
+++ Exercice1/Exercice1.py
@@ -3,50 +3,71 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 def Exercice1():
+    # Define possible values for X (age) and Y (hair color percentage)
+    age_values = np.array([20, 30, 40])
+    hair_color_percentages = np.array([0.2, 0.5, 0.3])
 
-# Define hair colors and corresponding probabilities
-    hair_colors = ["Blonde", "Brunette", "Red", "Black"]
-    probabilities = [0.2, 0.4, 0.2, 0.2]  # Example probabilities for each hair color
+    print("here is a representation of the hair color and the percentage of them in the population")
+    # Define possible values for Y (hair color percentage)
+    hair_color_categories = ['Blonde', 'Brown', 'Other']
+    hair_color_percentages = np.array([0.2, 0.5, 0.3])
 
-    # Number of points to sample
-    n_points = 100
+    # Plot the results with color
+    fig, ax = plt.subplots()
+    bars = ax.bar(hair_color_categories, hair_color_percentages, color=['#FFD700', '#964B00', '#808080'])
+    ax.set_ylabel('Hair Color Percentage')
+    ax.set_title('Hair Color Distribution')
 
-    # Number of iterations for convergence check
-    max_n = 1000
+    # Add percentages on top of the bars
+    for bar, percentage in zip(bars, hair_color_percentages):
+        height = bar.get_height()
+        ax.annotate(f'{percentage*100:.1f}%',
+                    xy=(bar.get_x() + bar.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
 
-    # Expected value calculation
-    expected_value = sum(p * i for p, i in zip(hair_colors, probabilities))
-
-    # Function to calculate Euclidean distance
-    def euclidean_distance(point1, point2):
-        return np.sqrt(np.sum((point1 - point2)**2))
-
-    # Sample n_points from the joint distribution
-    samples = np.random.choice(hair_colors, size=n_points, p=probabilities)
-
-    # Plot the sampled points
-    colors = ['gold' if color == 'Blonde' else 'brown' if color == 'Brunette' else 'red' if color == 'Red' else 'black' for color in samples]
-    plt.scatter(range(1, n_points + 1), np.random.rand(n_points), c=colors, alpha=0.6)
-    plt.title('Sampled Points from Joint Distribution')
-    plt.xlabel('Sample Number')
-    plt.ylabel('Random Value')
     plt.show()
 
-    # Initialize arrays to store results
-    n_values = np.arange(1, max_n + 1)
-    distances = np.zeros(max_n)
+    # Create a joint probability distribution
+    joint_distribution = np.array([[0.05, 0.1, 0.05],
+                                [0.1, 0.3, 0.1],
+                                [0.05, 0.1, 0.05]])
 
-    # Generate samples and calculate empirical averages
+    # Function to calculate Euclidean distance between two points
+    def euclidean_distance(point1, point2):
+        return np.linalg.norm(point1 - point2)
+
+    # Initialize arrays for storing results
+    n_values = np.arange(1, 1001)  # Values of n from 1 to 1000
+    distances = []
+
+    print("we sample it to 1000")
+    # Calculate expected value E[Z]
+    expected_value = np.sum(np.outer(age_values, hair_color_percentages) * joint_distribution)
+
+    print("the expected value is :" +str(expected_value))
+
+    # Sample n points and calculate empirical averages
     for n in n_values:
-        samples = np.random.choice(hair_colors, size=n, p=probabilities)
-        empirical_average = np.sum([1 if color == 'Blonde' else 2 if color == 'Brunette' else 3 if color == 'Red' else 4 for color in samples]) / n
-        distances[n - 1] = euclidean_distance(np.array([empirical_average]), np.array([expected_value]))
+        probabilities_x = np.sum(joint_distribution, axis=1) / np.sum(joint_distribution)
+        probabilities_y = np.sum(joint_distribution, axis=0) / np.sum(joint_distribution)
+        
+        samples_x = np.random.choice(age_values, size=n, p=probabilities_x)
+        samples_y = np.random.choice(hair_color_percentages, size=n, p=probabilities_y)
+        
+        samples = np.column_stack((samples_x, samples_y))
+        empirical_average = np.mean(samples, axis=0)
+        distance = euclidean_distance(empirical_average, expected_value)
+        distances.append(distance)
 
-    # Plot the results
-    plt.plot(n_values, distances)
-    plt.title('Convergence of Empirical Average to Expected Value')
-    plt.xlabel('Number of Samples (n)')
-    plt.ylabel('Euclidean Distance')
+    # Plot the results with color
+    fig, ax = plt.subplots()
+    sc = ax.scatter(n_values, distances, c=samples_y, cmap='viridis', marker='.')
+    fig.colorbar(sc, label='Hair Color Percentage')
+    ax.set_xlabel('Number of Samples (n)')
+    ax.set_ylabel('Euclidean Distance')
+    ax.set_title('Convergence of Empirical Average to Expected Value')
     plt.show()
 
 
---
+++ Exercice3/Exercice3.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_blobs
+from sklearn.metrics import pairwise_distances_argmin_min
+from sklearn.metrics import pairwise_distances
+from sklearn.decomposition import PCA
+from time import time
+
+def Exercice3():
+
+    # Load the dataset
+    data = np.load('Exercice3/data.npy')
+
+    # Standardize the dataset
+    scaler = StandardScaler()
+    data_scaled = scaler.fit_transform(data)
+
+    # Function to determine the optimal number of clusters using the Elbow Method
+    def elbow_method(data, max_clusters=10):
+        distortions = []
+        for i in range(1, max_clusters + 1):
+            kmeans = KMeans(n_clusters=i, random_state=42)
+            kmeans.fit(data)
+            distortions.append(kmeans.inertia_)
+        return distortions
+
+    # Function to determine the optimal number of clusters using the Silhouette Score
+    def silhouette_analysis(data, max_clusters=10):
+        silhouette_scores = []
+        for i in range(2, max_clusters + 1):
+            kmeans = KMeans(n_clusters=i, random_state=42)
+            labels = kmeans.fit_predict(data)
+            silhouette_avg = silhouette_score(data, labels)
+            silhouette_scores.append(silhouette_avg)
+        return silhouette_scores
+
+    # Function to perform clustering using KMeans
+    def perform_kmeans(data, n_clusters):
+        kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+        labels = kmeans.fit_predict(data)
+        return labels
+
+    # Function to perform clustering using Agglomerative Clustering
+    def perform_agglomerative(data, n_clusters):
+        agglomerative = AgglomerativeClustering(n_clusters=n_clusters)
+        labels = agglomerative.fit_predict(data)
+        return labels
+
+    # Perform Elbow Method and Silhouette Analysis
+    max_clusters = 10
+    distortions = elbow_method(data_scaled, max_clusters)
+    silhouette_scores = silhouette_analysis(data_scaled, max_clusters)
+
+    # Plot Elbow Method
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, max_clusters + 1), distortions, marker='o')
+    plt.title('Elbow Method')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Distortion')
+
+    # Plot Silhouette Score
+    plt.subplot(1, 2, 2)
+    plt.plot(range(2, max_clusters + 1), silhouette_scores, marker='o')
+    plt.title('Silhouette Analysis')
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Silhouette Score')
+
+    plt.tight_layout()
+    plt.show()
+
+    # Choose the optimal number of clusters based on the analysis
+    optimal_clusters_kmeans = np.argmin(distortions) + 1  # Elbow Method
+    optimal_clusters_agglomerative = np.argmax(silhouette_scores) + 2  # Silhouette Analysis
+
+    # Perform clustering with optimal number of clusters
+    labels_kmeans = perform_kmeans(data_scaled, optimal_clusters_kmeans)
+    labels_agglomerative = perform_agglomerative(data_scaled, optimal_clusters_agglomerative)
+
+    # Plot the clustered data
+    plt.figure(figsize=(12, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.scatter(data_scaled[:, 0], data_scaled[:, 1], c=labels_kmeans, cmap='viridis', edgecolor='k', s=40)
+    plt.title(f'KMeans Clustering (k={optimal_clusters_kmeans})')
+
+    plt.subplot(1, 2, 2)
+    plt.scatter(data_scaled[:, 0], data_scaled[:, 1], c=labels_agglomerative, cmap='viridis', edgecolor='k', s=40)
+    plt.title(f'Agglomerative Clustering (k={optimal_clusters_agglomerative})')
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    print("[EX3] Cluster and heuristic...")
+    start = time()
+    Exercice3()
+    print(f"[EX3] Done - {(time() - start):0.4f}s")
